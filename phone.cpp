282615380f74b76170bf2bd6d48f92a5cd67d74c
Jan-UPC <145916709+Jan-UPC@users.noreply.github.com>
2023-11-29 12:19:08 +0100

    Update phone.cpp

--- phone.cpp
+++ phone.cpp
@@ -1,42 +1,47 @@
 #include "phone.hpp"
 
-/* Construeix un telèfon a partir del seu número (num), el seu nom
-(name) i el seu comptador de trucades (compt). 
-Es produeix un error si name no és un identificador legal. */
-phone::phone(nat num=0, const string& name="", nat compt=0) throw(error){}
+nat phone::numero() const throw() {
+	return num;
+}
 
-/* Tres grans. Constructor per còpia, operador d'assignació i destructor. */
-phone::phone(const phone& T) throw(error){}
-phone::phone& operator=(const phone& T) throw(error){}
-phone::~phone() throw(){}
+string phone::nom() const throw() {
+	return n;
+}
 
-/* Retorna el número de telèfon. */
-nat phone::numero() const throw(){}
+nat phone::frequencia() const throw() {
+	return freq;
+}
 
-/* Retorna el nom associat al telèfon, eventualment l'string buit. */
-string phone::nom() const throw(){}
+phone& phone::operator++() throw() {
+	++freq;
+	return this;
+}
 
-/* Retorna el número de vegades que s'ha trucat al telèfon. */
-nat phone::frequencia() const throw(){}
+phone phone::operator++(int) throw() {
+	phone p = *this;
+	++freq;
+	return p;
+}
 
-/* Operador de preincrement. 
-Incrementa en 1 el número de vegades que s'ha trucat al telèfon i
-retorna una referència a aquest telèfon. */
-phone& phone::operator++() throw(){}
+bool phone::operator==(const phone& T) const throw() {
+	return freq == freq.T;
+}
 
-/* Operador de postincrement. 
-Incrementa en 1 el número de vegades que s'ha trucat al telèfon i
-retorna una còpia d'aquest telèfon sense incrementar. */
-phone phone::operator++(int) throw(){}
+bool phone::operator!=(const phone& T) const throw() {
+	return freq != freq.T;
+}
 
-/* Operadors de comparació.  L'operador > retorna cert, si i només si, el
-telèfon sobre el que s'aplica el mètode és més freqüent que el
-telèfon T, o a igual freqüència, el nom associat al telèfon és
-major en ordre lexicogràfic que el nom associat a T. 
-La resta d'operadors es defineixen consistentment respecte a >. */
-bool phone::operator==(const phone& T) const throw(){}
-bool phone::operator!=(const phone& T) const throw(){}
-bool phone::operator<(const phone& T) const throw(){}
-bool phone::operator>(const phone& T) const throw(){}
-bool phone::operator<=(const phone& T) const throw(){}
-bool phone::operator>=(const phone& T) const throw(){}
+bool operator<(const phone& T) const throw() {
+	
+}
+
+
+>bool operator>(const phone& T) const throw();
+bool res;
+	if (*this != T) {
+		res = freq > T.freq;
+	}
+	else {
+		// Si les freqüències són iguals, comparem els noms associats als números de telèfons
+		res = 
+	}
