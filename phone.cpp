2cdae9657311937a4a70ad58fae125df8fce474c
Jan-UPC <145916709+Jan-UPC@users.noreply.github.com>
2023-12-17 18:53:40 +0100

    Update phone.cpp
    Se han implementado todos los métodos de la clase phone

--- phone.cpp
+++ phone.cpp
@@ -1,47 +1,111 @@
 #include "phone.hpp"
 
+phone::phone(nat num, const string& name, nat compt) throw(error) : _num(num), _freq(compt) {
+	nat mida = name.size();
+	for (nat i = 0; i < mida; ++i) {
+		if (name[i] == DELETECHAR or name[i] == ENDCHAR or name[i] == ENDPREF) {
+			throw ErrNomIncorrecte;
+		}
+	}
+	// Si tots els caràcters de name són legals, no es produeix cap error
+	_n = name;
+}
+
+phone::phone(const phone& T) throw(error) {
+	_num = T._num;
+	_n = T._n;
+	_freq = T._freq;
+}
+
+phone& phone::operator=(const phone& T) throw(error) {
+	if (this != &T) {
+		_num = T._num;
+		_n = T._n;
+		_freq = T._freq;
+	}
+	return *this;
+}
+
+phone::~phone() throw() {
+}
+
 nat phone::numero() const throw() {
-	return num;
+	return _num;
 }
 
 string phone::nom() const throw() {
-	return n;
+	return _n;
 }
 
 nat phone::frequencia() const throw() {
-	return freq;
+	return _freq;
 }
 
 phone& phone::operator++() throw() {
-	++freq;
-	return this;
+	++_freq;
+	return *this;
 }
 
 phone phone::operator++(int) throw() {
 	phone p = *this;
-	++freq;
+	++_freq;
 	return p;
 }
 
 bool phone::operator==(const phone& T) const throw() {
-	return freq == freq.T;
+	return _freq == T._freq;
 }
 
 bool phone::operator!=(const phone& T) const throw() {
-	return freq != freq.T;
+	return _freq != T._freq;
 }
 
-bool operator<(const phone& T) const throw() {
-	
-}
-
-
->bool operator>(const phone& T) const throw();
-bool res;
-	if (*this != T) {
-		res = freq > T.freq;
+bool phone::operator<(const phone& T) const throw() {
+	bool res;
+	if (*this != T) { // Si les freqüències són diferents, les comparem
+		res = _freq < T._freq;
 	}
 	else {
 		// Si les freqüències són iguals, comparem els noms associats als números de telèfons
-		res = 
+		res = _n < T._n;
 	}
+	return res;
+}
+
+
+bool phone::operator>(const phone& T) const throw() {
+	bool res;
+	if (*this != T) { // Si les freqüències són diferents, les comparem
+		res = _freq > T._freq;
+	}
+	else {
+		// Si les freqüències són iguals, comparem els noms associats als números de telèfons
+		res = _n > T._n;
+	}
+	return res;
+}
+
+bool phone::operator<=(const phone& T) const throw() {
+	bool res;
+	if (*this != T) { // Si les freqüències són diferents, les comparem
+		res = _freq <= T._freq;
+	}
+	else {
+		// Si les freqüències són iguals, comparem els noms associats als números de telèfons
+		res = _n <= T._n;
+	}
+	return res;
+}
+
+bool phone::operator>=(const phone& T) const throw() {
+	bool res;
+	if (*this != T) { // Si les freqüències són diferents, les comparem
+		res = _freq <= T._freq;
+	}
+	else {
+		// Si les freqüències són iguals, comparem els noms associats als números de telèfons
+		res = _n <= T._n;
+	}
+	return res;
+}
+
