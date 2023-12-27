25ae4ffc1d9e4171c2c37ed1ca0332ae846b64a1
Markus <marcgasensio@icloud.com>
2023-12-28 00:10:15 +0100

    1. Hay partes de codigo repetido que podrian ser substiuida por funciones auxiliares, 2. Assignacion de nombre mejorable añadiendo funcion nen phone, 3. Posible funcion crea_nodo para mejorar codigo, 4. Faltan codigos de error, 5. Habria que cambiar algunos int por nat, 6. Muchas maneras de crear dump().

--- call_registry.cpp
+++ call_registry.cpp
@@ -27,6 +27,11 @@ void call_registry::esborra_nodes(node_taula* p) {
 	}
 }
 
+int call_registry::hash(const nat &x) {
+	static long const MULT = 31415926;
+	long int y = ((x * x * MULT) << 20) >> 4;
+	return y;
+}
 
 call_registry::call_registry() throw(error) {
 	_M = 5;
@@ -71,37 +76,142 @@ call_registry::~call_registry() throw() {
 }
 
 void call_registry::registra_trucada(nat num) throw(error) {
-	//...
+	int i = hash(num);
+	node_taula* t = _taula[i];
+	bool hi_es = false;
+	while (t != nullptr and not hi_es) {
+		if (t->_k == num) { 
+			hi_es = true;
+			t->_p++;
+		} else {
+			t = t->_seg; 
+		}
+	}
+	if(not hi_es){
+		node_taula* nou = new node_taula;
+		string buit = "";
+		phone p(num, "", 1);
+		nou->_p = p;
+		nou->_k = num;
+		_taula[i] = nou; 
+		++_nelem;
+	}
 }
 
 void call_registry::assigna_nom(nat num, const string& name) throw(error) {
-	//...
+	int i = hash(num);
+	node_taula* t = _taula[i];
+	bool hi_es = false;
+	while (t != nullptr and not hi_es) {
+		if (t->_k == num) { 
+			hi_es = true;
+		} else {
+			t = t->_seg; 
+		}
+	}
+	if(not hi_es){
+		node_taula* nou = new node_taula;
+		phone p(num, name, 0);
+		nou->_p = p;
+		nou->_k = num;
+		_taula[i] = nou; 
+		++_nelem;
+	} else {
+		nat freq = t->_p.frequencia();
+		phone p(num, name, freq);
+		t->_p = p;
+	}
 }
 
-void elimina(nat num) throw(error) {
-	//...
+void call_registry::elimina(nat num) throw(error) {
+	nat i = hash(num);
+	node_taula *p = _taula[i], *ant=nullptr; 
+	bool trobat = false;
+	while (p != nullptr and not trobat) {
+		if (p->_k == num) { 
+			trobat = true;
+		} else {
+			ant = p;
+			p = p->_seg; 
+		}
+	}
+	if (trobat) {
+		if (ant == nullptr) { 
+			_taula[i] = p->_seg;
+		} else {
+			ant->_seg = p->_seg; 
+		}
+		delete(p);
+		--_nelem; 
+	} else {
+		throw error(ErrNumeroInexistent);
+	}
 }
 
 bool call_registry::conte(nat num) const throw() {
-	//...
+	int i = hash(num);
+	node_taula* t = _taula[i];
+	bool hi_es = false;
+	while (t != nullptr and not hi_es) {
+		if (t->_k == num) { 
+			hi_es = true;
+		} else {
+			t = t->_seg; 
+		}
+	}
+	return hi_es;
 }
 
 string call_registry::nom(nat num) const throw(error) {
-	//...
+	int i = hash(num);
+	string nom = "";
+	node_taula* t = _taula[i];
+	bool hi_es = false;
+	while (t != nullptr and not hi_es) {
+		if (t->_k == num) { 
+			hi_es = true;
+		} else {
+			t = t->_seg; 
+		}
+	}
+	if (hi_es) {
+		nom = t->_p.nom();
+	} else {
+		throw error(ErrNumeroInexistent);
+	}
+	return nom;
 }
 
 nat call_registry::num_trucades(nat num) const throw(error) {
-	//...
+	int i = hash(num);
+	nat freq;
+	node_taula* t = _taula[i];
+	bool hi_es = false;
+	while (t != nullptr and not hi_es) {
+		if (t->_k == num) { 
+			hi_es = true;
+		} else {
+			t = t->_seg; 
+		}
+	}
+	if (hi_es) {
+		freq = t->_p.frequencia();
+	} else {
+		throw error(ErrNumeroInexistent);
+	}
+	return freq;
 }
 
 bool call_registry::es_buit() const throw() {
-	//...
+	return _nelem==0;
 }
 
 nat call_registry::num_entrades() const throw() {
-	//...
+	return _nelem;
 }
 
 void call_registry::dump(vector<phone>& V) const throw(error) {
-	//...
+	for(int i = 0; i<_nelem; ++i){
+		// Por implementar ya que hay varias opciones a mirar.
+	}
 }
